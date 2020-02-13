ea355d88aa39bb7e7a17ef2eee64b9ec1f163829
ibrahimshamma99 <i.abushammah@gmail.com>
2020-02-13 22:12:28 +0200

    build ferris

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let out = b"Hello fellow Rustaceans!";
+    let width = 24;
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(out, width, &mut writer).unwrap();
 }
